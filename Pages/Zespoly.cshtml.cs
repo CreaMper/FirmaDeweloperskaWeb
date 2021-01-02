b64a6923f49dc95c7cd13e831ba4c028b3d8ee6e
Arkadiusz Wieruchowski <arkadiusz.wieruchowski@outlook.com>
2021-01-02 13:13:06 +0100

    Dodano widok zespolów

--- Pages/Zespoly.cshtml.cs
+++ Pages/Zespoly.cshtml.cs
@@ -9,6 +9,14 @@ namespace FirmaDeweloperskaWeb.Pages
 {
     public class ZespolyModel : PageModel
     {
+        public firma_deweloperska_3Context db { set; get;}
+        public List<Zespoly> zespoly { get; set; }
+        public List<Zespoly> zespolyZlecenia { get; set; }
+        public ZespolyModel (firma_deweloperska_3Context _context)
+        {
+            db = _context;
+            zespoly = (from zespoly in this.db.Zespolies select zespoly).ToList();
+        }
         public void OnGet()
         {
         }
